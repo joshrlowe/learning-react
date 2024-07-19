cd237faab191c6932115fda64c1cc69721712f7e
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-19 13:07:16 -0400

    18 Revisiting Operators

---
+++ 2-javascript-refresher/18 Revisiting Operators/assets/scripts/app.js
@@ -0,0 +1,10 @@
+console.log("Hello World!"); // Prints "Hello World!" to the console.
+console.log("Hello " + "World!"); // Concatenates two strings and prints "Hello World!" to the console.
+console.log(10 === "10"); // Strict equality check, returns false because data types are different.
+console.log(10 === 10); // Strict equality check, returns true.
+console.log(10 == 10); // Type-converting equality check, returns true.
+console.log(10 > 5); // Checks if 10 is greater than 5, returns true.
+
+if (10 === 10) {
+  console.log("works"); // This block executes because the if-condition is true, prints "works".
+}
