a8e0589836658120fba0d6a7d10fcb1bbc0cde5a
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-22 10:30:03 -0400

    51 Passing Functions as Values to Props

---
+++ 3-react-essentials/51 Passing Functions as Values to Props/src/App.jsx
@@ -0,0 +1,38 @@
+import { CORE_CONCEPTS } from "./data.js";
+import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  function handleSelect() {
+    console.log("Selected");
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Component</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
