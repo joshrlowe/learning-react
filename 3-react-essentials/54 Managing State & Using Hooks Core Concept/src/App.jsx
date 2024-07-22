520682876a1be833b91a78617a1a8efcb5af7f7f
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-22 10:31:33 -0400

    54 Managing State & Using Hooks Core Concept

---
+++ 3-react-essentials/54 Managing State & Using Hooks Core Concept/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import { CORE_CONCEPTS } from "./data.js";
+import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  console.log("App Component Rendering");
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Component
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          <p>{selectedTopic}</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
