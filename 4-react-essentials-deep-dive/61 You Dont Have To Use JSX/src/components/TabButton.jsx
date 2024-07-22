79b26e3881659b975dd04f331e4db8bf11782c4c
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-22 13:54:44 -0400

    61 You Dont Have To Use JSX

---
+++ 4-react-essentials-deep-dive/61 You Dont Have To Use JSX/src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton({ children, onSelect, isSelected }) {
+  console.log("TabButton Component Rendering");
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
+}
