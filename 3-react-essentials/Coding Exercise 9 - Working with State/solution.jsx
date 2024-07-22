7d0fffd139255155ae8e5f58754a4a289712c05f
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-22 10:31:33 -0400

    Coding Exercise 9 - Working with State

---
+++ 3-react-essentials/Coding Exercise 9 - Working with State/solution.jsx
@@ -0,0 +1,24 @@
+// IMPORTANT: You CAN'T import & use useState like this in this Udemy environment
+// import { useState } from 'react'
+// Instead, import & use it like this:
+// import React from 'react';
+// React.useState();
+
+import React from "react";
+
+export default function App() {
+  const [price, setPrice] = React.useState(100);
+
+  return (
+    <div>
+      <p data-testid="price">${price}</p>
+      <button
+        onClick={() => {
+          setPrice(75);
+        }}
+      >
+        Apply Discount
+      </button>
+    </div>
+  );
+}
