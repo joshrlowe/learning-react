419567df2ca897470cde15a0d69c7af6312d918f
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-30 00:32:36 -0400

    81 Best Practice Updating Object State Immutably

---
+++ 4-react-essentials-deep-dive/81 Best Practice Updating Object State Immutably/src/components/GameBoard.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex, playerSymbol) {
+    setGameBoard((prevGameBoard) => {
+      const newGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      newGameBoard[rowIndex][colIndex] =
+        newGameBoard[rowIndex][colIndex] === null
+          ? "X"
+          : newGameBoard[rowIndex][colIndex];
+      return newGameBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default GameBoard;
