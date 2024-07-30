2ca91eb60e95dc6b6c6497aece27b165a312a640
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-29 21:32:14 -0400

    77 Conditional Content & A Suboptimal Way Of Updating State

---
+++ 4-react-essentials-deep-dive/77 Conditional Content & A Suboptimal Way Of Updating State/src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnContent = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    btnContent = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnContent}</button>
+    </li>
+  );
+}
+
+export default Player;
