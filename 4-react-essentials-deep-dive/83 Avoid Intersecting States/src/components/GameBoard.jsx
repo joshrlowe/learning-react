506fff57654f404ed64db1a6de12c26fc929f1f7
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-30 00:32:36 -0400

    83 Avoid Intersecting States

---
+++ 4-react-essentials-deep-dive/83 Avoid Intersecting States/src/components/GameBoard.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex, activePlayerSymbol) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const newGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //     newGameBoard[rowIndex][colIndex] = newGameBoard[rowIndex][colIndex] === null ? activePlayerSymbol : newGameBoard[rowIndex][colIndex];
+  //     return newGameBoard;
+  //   });
+
+  //   onSelectSquare();
+  // }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() =>
+                    handleSelectSquare(rowIndex, colIndex, activePlayerSymbol)
+                  }
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default GameBoard;
