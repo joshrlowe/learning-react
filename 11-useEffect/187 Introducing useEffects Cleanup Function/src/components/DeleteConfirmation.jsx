2eba63d96071bba8531bbe2d7caa58595c7fe573
Josh Lowe <joshlowe.cs@gmail.com>
2024-08-05 11:50:09 -0400

    187 Introducing useEffects Cleanup Function

---
+++ 11-useEffect/187 Introducing useEffects Cleanup Function/src/components/DeleteConfirmation.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    /*
+      The cleanup function is what is returned from the useEffect hook.
+      This cleanup function is called before the component is unmounted.
+      It runs on every render except the first one.
+     */
+
+    return () => {
+      console.log("Timer Cleared");
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <div id="delete-confirmation">
+      <h2>Are you sure?</h2>
+      <p>Do you really want to remove this place?</p>
+      <div id="confirmation-actions">
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
+        <button onClick={onConfirm} className="button">
+          Yes
+        </button>
+      </div>
+    </div>
+  );
+}
