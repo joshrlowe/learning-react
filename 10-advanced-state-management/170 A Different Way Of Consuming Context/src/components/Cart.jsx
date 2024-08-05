260d90c46d228da3a59059fe6d09f7b43e0f3bc2
Josh Lowe <joshlowe.cs@gmail.com>
2024-08-04 20:34:47 -0400

    170 A Different Way Of Consuming Context

---
+++ 10-advanced-state-management/170 A Different Way Of Consuming Context/src/components/Cart.jsx
@@ -0,0 +1,62 @@
+import CartContext from "../store/shopping-cart-context";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  return (
+    /*
+      Instead of using the useContext hook, we can use the CartContext.Consumer component.
+      A function is passed as a child to this component, and the function recieves the context value as an argument.
+          In our case, this function recieves the cartCtx object as an argument.
+      This is the cumbersome approach and can be hard to read, which is why it't not the default approach to using context.
+      It is something you should be aware of, though.
+
+      Going forward, we will use the useContext hook to consume context.
+     */
+    <CartContext.Consumer>
+      {(cartCtx) => {
+        const totalPrice = cartCtx.items.reduce(
+          (acc, item) => acc + item.price * item.quantity,
+          0,
+        );
+        const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+
+        return (
+          <div id="cart">
+            {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+            {cartCtx.items.length > 0 && (
+              <ul id="cart-items">
+                {cartCtx.items.map((item) => {
+                  const formattedPrice = `$${item.price.toFixed(2)}`;
+
+                  return (
+                    <li key={item.id}>
+                      <div>
+                        <span>{item.name}</span>
+                        <span> ({formattedPrice})</span>
+                      </div>
+                      <div className="cart-item-actions">
+                        <button
+                          onClick={() => onUpdateItemQuantity(item.id, -1)}
+                        >
+                          -
+                        </button>
+                        <span>{item.quantity}</span>
+                        <button
+                          onClick={() => onUpdateItemQuantity(item.id, 1)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            <p id="cart-total-price">
+              Cart Total: <strong>{formattedTotalPrice}</strong>
+            </p>
+          </div>
+        );
+      }}
+    </CartContext.Consumer>
+  );
+}
